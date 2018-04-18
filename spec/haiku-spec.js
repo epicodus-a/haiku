2091c53e74e1b873d20427ba96787fcf86d63899
Adriana C and Jessica Schmitz <adriana@>
2018-04-18 13:25:48 -0700

    create test for doubleCons

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,6 +1,7 @@
 import {Haiku} from "../src/Haiku";
 
 describe("Haiku", function(){
+  debugger;
   let reuseableHaiku;
   beforeEach(function(){
     reuseableHaiku = new Haiku("apple ", "banana", "orange");
@@ -16,8 +17,8 @@ describe("Haiku", function(){
     // let reuseableHaiku = new Haiku("apple ", "banana", "orange");
     expect(Array.isArray(reuseableHaiku.inputArr(reuseableHaiku.line1))).toBeTruthy();
   });
-  
+
   it("should return a word that does not have consecutive consonants", function() {
-    expect(reuseableHaiku.seperateDoubleConsonant("tea")).toBeTruthy();
+    expect(reuseableHaiku.doubleCons("appe").length).toEqual(2);
   });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,3 +1,5 @@
+// export {Haiku};
+
 export class Haiku{
   constructor(line1, line2, line3){
     this.line1 = line1;
@@ -10,9 +12,28 @@ export class Haiku{
     // return: an array
     return aStr.split(" ");
   }
-  //
-  // seperateDoubleConsonant(aWord){
-  //   let consonants = "bcdfghjklmnpqrstvwxz";
-  //   this.inputArr(aWord).forEach()
-  // }
+
+
+  doubleCons(aWord){
+    // aWord: a string
+    // return: an array of words which does not contain consecutive consonants
+    debugger;
+    let consonants = "bcdfghjklmnpqrstvwxz";
+    let singleConsWordsArr = [];
+    let doubleCons = false;
+    for(let i = 0; i < aWord.length - 1 ; i ++){
+      if(consonants.indexOf(aWord[i]) !== -1 ){
+        if(consonants.indexOf(aWord[i+1]) !== -1){
+          singleConsWordsArr.push(aWord.substring(0, i+1));
+          singleConsWordsArr.push(aWord.substring(i+1));
+          doubleCons = true;
+        }
+      }
+    }
+    if(!doubleCons){
+      singleConsWordsArr.push(aWord);
+    }
+    return singleConsWordsArr;
+  }
+
 }
